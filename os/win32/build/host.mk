17f10dd11bee2bdf0cf37fd7a46041c0d5feedea
johnbr <john.bradley@datalight.com>
2015-06-30 13:22:05 -0700

    Trunk Build 641

--- os/win32/build/host.mk
+++ os/win32/build/host.mk
@@ -1,6 +1,7 @@
 CC=cl
+LD=link
 P_OS ?= win32
-B_OBJEXT ?= obj
+B_OBJEXT ?= tobj
 
 INCLUDES=					\
 	/I $(P_PROJDIR)				\
@@ -17,7 +18,7 @@ endif
 
 all: redfmt redimgbld
 
-%.obj: %.c
+%.$(B_OBJEXT): %.c
 	$(CC) $(EXTRA_CFLAGS) $(INCLUDES) $< /c /Fo$@
 
 # The redconf.h for the tools #includes the redconf.h from the parent project
@@ -26,41 +27,41 @@ REDPROJHDR=$(P_CONFDIR)/redconf.h
 
 include $(P_BASEDIR)/build/reliance.mk
 
-IMGBLDHDR=						\
-	$(P_BASEDIR)/os/win32/tools/imgbld/ibheader.h	\
+IMGBLDHDR=							\
+	$(P_BASEDIR)/os/win32/tools/imgbld/ibheader.h		\
 	$(P_BASEDIR)/os/win32/tools/wintlcmn.h
-IMGBLDOBJ=						\
-	$(P_BASEDIR)/os/win32/tools/imgbld/ibcommon.obj	\
-	$(P_BASEDIR)/os/win32/tools/imgbld/ibfse.obj	\
-	$(P_BASEDIR)/os/win32/tools/imgbld/ibposix.obj	\
-	$(P_BASEDIR)/os/win32/tools/imgbld/imgbld.obj	\
-	$(P_BASEDIR)/os/win32/tools/wintlcmn.obj
-REDPROJOBJ=						\
-	$(IMGBLDOBJ)					\
-	$(P_BASEDIR)/os/win32/tools/imgcopy.obj		\
-	$(P_BASEDIR)/os/win32/tools/winchk.obj		\
-	$(P_BASEDIR)/os/win32/tools/winfmt.obj
+IMGBLDOBJ=							\
+	$(P_BASEDIR)/os/win32/tools/imgbld/ibcommon.$(B_OBJEXT)	\
+	$(P_BASEDIR)/os/win32/tools/imgbld/ibfse.$(B_OBJEXT)	\
+	$(P_BASEDIR)/os/win32/tools/imgbld/ibposix.$(B_OBJEXT)	\
+	$(P_BASEDIR)/os/win32/tools/imgbld/imgbld.$(B_OBJEXT)	\
+	$(P_BASEDIR)/os/win32/tools/wintlcmn.$(B_OBJEXT)
+REDPROJOBJ=							\
+	$(IMGBLDOBJ)						\
+	$(P_BASEDIR)/os/win32/tools/imgcopy.$(B_OBJEXT)		\
+	$(P_BASEDIR)/os/win32/tools/winchk.$(B_OBJEXT)		\
+	$(P_BASEDIR)/os/win32/tools/winfmt.$(B_OBJEXT)
 
-$(P_BASEDIR)/os/win32/tools/imgbld/ibcommon.obj:	$(P_BASEDIR)/os/win32/tools/imgbld/ibcommon.c $(REDHDR) $(IMGBLDHDR)
-$(P_BASEDIR)/os/win32/tools/imgbld/ibfse.obj:		$(P_BASEDIR)/os/win32/tools/imgbld/ibfse.c $(REDHDR) $(IMGBLDHDR)
-$(P_BASEDIR)/os/win32/tools/imgbld/ibposix.obj:		$(P_BASEDIR)/os/win32/tools/imgbld/ibposix.c $(REDHDR) $(IMGBLDHDR)
-$(P_BASEDIR)/os/win32/tools/imgbld/imgbld.obj:		$(P_BASEDIR)/os/win32/tools/imgbld/imgbld.c $(REDHDR) $(IMGBLDHDR)
-$(P_BASEDIR)/os/win32/tools/winfmt.obj:			$(P_BASEDIR)/os/win32/tools/winfmt.c $(REDHDR) $(P_BASEDIR)/os/win32/tools/wintlcmn.h
-$(P_BASEDIR)/os/win32/tools/wintlcmn.obj:		$(P_BASEDIR)/os/win32/tools/wintlcmn.c  $(REDHDR) $(P_BASEDIR)/os/win32/tools/wintlcmn.h
+$(P_BASEDIR)/os/win32/tools/imgbld/ibcommon.$(B_OBJEXT):	$(P_BASEDIR)/os/win32/tools/imgbld/ibcommon.c $(REDHDR) $(IMGBLDHDR)
+$(P_BASEDIR)/os/win32/tools/imgbld/ibfse.$(B_OBJEXT):		$(P_BASEDIR)/os/win32/tools/imgbld/ibfse.c $(REDHDR) $(IMGBLDHDR)
+$(P_BASEDIR)/os/win32/tools/imgbld/ibposix.$(B_OBJEXT):		$(P_BASEDIR)/os/win32/tools/imgbld/ibposix.c $(REDHDR) $(IMGBLDHDR)
+$(P_BASEDIR)/os/win32/tools/imgbld/imgbld.$(B_OBJEXT):		$(P_BASEDIR)/os/win32/tools/imgbld/imgbld.c $(REDHDR) $(IMGBLDHDR)
+$(P_BASEDIR)/os/win32/tools/winfmt.$(B_OBJEXT):			$(P_BASEDIR)/os/win32/tools/winfmt.c $(REDHDR) $(P_BASEDIR)/os/win32/tools/wintlcmn.h
+$(P_BASEDIR)/os/win32/tools/wintlcmn.$(B_OBJEXT):		$(P_BASEDIR)/os/win32/tools/wintlcmn.c  $(REDHDR) $(P_BASEDIR)/os/win32/tools/wintlcmn.h
 
 # The redconf.c for the tools #includes the redconf.c from the parent project
 # to inherit its settings, so add it as a dependency.
-$(P_PROJDIR)/redconf.obj:	$(P_CONFDIR)/redconf.c
+$(P_PROJDIR)/redconf.$(B_OBJEXT):	$(P_CONFDIR)/redconf.c
 
 
-redfmt: $(P_BASEDIR)/os/win32/tools/winfmt.obj $(P_BASEDIR)/os/win32/tools/wintlcmn.obj $(REDDRIVOBJ) $(REDTOOLOBJ)
-	$(CC) $(EXTRA_CFLAGS) $^ /Fe$@.exe
+redfmt: $(P_BASEDIR)/os/win32/tools/winfmt.$(B_OBJEXT) $(P_BASEDIR)/os/win32/tools/wintlcmn.$(B_OBJEXT) $(REDDRIVOBJ) $(REDTOOLOBJ)
+	$(LD) /OUT:$@.exe $^
 
 redimgbld: $(IMGBLDOBJ) $(REDDRIVOBJ) $(REDTOOLOBJ)
-	$(CC) $(EXTRA_CFLAGS) $^ /Fe$@.exe
+	$(LD) /OUT:$@.exe $^
 
 .phony: clean
 clean:
 	del /f /q $(subst /,\,$(REDDRIVOBJ) $(REDTOOLOBJ) $(REDPROJOBJ)) 2>NUL
-	del /f /q *.ilk *.pdb *.obj *.exe *.suo *.sln 2>NUL
+	del /f /q *.ilk *.pdb *.$(B_OBJEXT) *.exe *.suo *.sln 2>NUL
 

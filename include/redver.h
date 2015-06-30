9acfefd009c0db284e564f1abb3db858467777ca
johnbr <john.bradley@datalight.com>
2015-06-30 15:01:54 -0700

    Trunk Build 644

--- include/redver.h
+++ include/redver.h
@@ -33,7 +33,7 @@
 
     <!-- This macro is updated automatically: do not edit! -->
 */
-#define RED_BUILD_NUMBER "643"
+#define RED_BUILD_NUMBER "644"
 
 #define RED_KIT_GPL         0U  /* Open source GPL kit. */
 #define RED_KIT_COMMERCIAL  1U  /* Commercially-licensed kit. */

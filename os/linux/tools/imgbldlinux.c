d536d1062aa7081cffacd8ad09c97905743544bc
Aaron Campbell <aaron.campbell@datalight.com>
2016-12-05 17:27:58 -0800

    Fix string copy checks

--- os/linux/tools/imgbldlinux.c
+++ os/linux/tools/imgbldlinux.c
@@ -144,11 +144,9 @@ static int FtwCopyFile(
             }
             else
             {
-                (void) strncpy(mapping.asInFilePath, pszPath,
-                               sizeof(mapping.asInFilePath) - 1);
-                mapping.asInFilePath[sizeof(mapping.asInFilePath) - 1] = '\0';
+                strcpy(mapping.asInFilePath, pszPath);
                 ret = IbConvertPath(gVolName, pszPath, gBaseDir, mapping.asOutFilePath);
-                
+
                 if(ret == 0)
                 {
                     ret = IbCopyFile(-1, &mapping);
@@ -328,6 +326,11 @@ int IbSetRelativePath(
             fprintf(stderr, "Error: paths in mapping file must be absolute if no input directory is specified.\n");
             ret = -1;
         }
+        else if(strlen(pszPath) >= HOST_PATH_MAX)
+        {
+            fprintf(stderr, "Error: path too long: %s\n", pszPath);
+            ret = -1;
+        }
         else
         {
             char    asTemp[HOST_PATH_MAX];
@@ -337,8 +340,7 @@ int IbSetRelativePath(
 
             REDASSERT(indirLen != 0);
 
-            (void) strncpy(asTemp, pszPath, HOST_PATH_MAX - 1);
-            asTemp[HOST_PATH_MAX - 1] = '\0';
+            strcpy(asTemp, pszPath);
 
             /*  Ensure a path separator comes between the input directory
                 and the specified relative path.

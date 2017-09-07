4129ed60e8b06071ad8c1f4615b3d0713d6aba27
johnbr <john.bradley@datalight.com>
2017-09-06 17:15:19 -0700

    Trunk Build 714

--- include/redver.h
+++ include/redver.h
@@ -33,7 +33,7 @@
 
     <!-- This macro is updated automatically: do not edit! -->
 */
-#define RED_BUILD_NUMBER "713"
+#define RED_BUILD_NUMBER "714"
 
 #define RED_KIT_GPL         0U  /* Open source GPL kit. */
 #define RED_KIT_COMMERCIAL  1U  /* Commercially-licensed kit. */

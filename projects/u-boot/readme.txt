e0024d29b761550e57cfe68d1814d330b92a00b3
johnbr <john.bradley@datalight.com>
2018-05-31 13:43:36 -0700

    Trunk Build 761

--- projects/u-boot/readme.txt
+++
@@ -1,2 +0,0 @@
-Please refer to the U-Boot Integration chapter in the Developer's Guide.
-
--- projects/win32/host/redconf.c
+++
@@ -1,7 +0,0 @@
-/** @file
-*/
-
-/*  The host tools use the same volume configuration as the target.
-*/
-#include "../redconf.c"
-
